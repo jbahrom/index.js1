5fd2a952fb2b73a18378df3de494474a81393892
jbahrom <bahromjorayev33@gmail.com>
2021-06-17 20:22:08 +0300

    java script

---
+++ index.js
@@ -0,0 +1,53 @@
+var b=4;
+var a=10;
+var p=b*a;
+
+console.log(p)
+
+var a=4;
+var s=a;
+console.log(s)
+
+var a=5;
+var b=8;
+var s=b*a;
+
+var d=2;
+var a=6;
+var b=11;
+var p=a*(d+b)
+
+console.log(s,p);
+
+var d=64;
+var p=79;
+var l=d*p;
+
+console.log(l);
+
+var a=12;
+var b=6;
+var c=29;
+var v=a*b*c;
+console.log(v);
+
+var a=20;
+var b=89;
+var c=57;
+var d=2;
+var s=d*(a*b+b*c+a*c)
+
+console.log(s);
+
+var a=28;
+var b=78;
+var d=2;
+
+console.log((a+b)/d);
+
+
+
+
+
+
+
