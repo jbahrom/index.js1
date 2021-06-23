a19b2c8a9e906650116c84962ee11a19f82d571e
jbahrom <bahromjorayev33@gmail.com>
2021-06-23 16:55:36 +0300

    js

---
+++ index1.js
@@ -0,0 +1,21 @@
+
+var bal=47;
+
+if( bal >=95 ) {
+    console.log( `siz balinigiz A` ) ;
+} else if ( bal < 90 && bal >= 80 ) {
+    console.log( `siz balinigiz B` ) ;
+} else if ( bal < 80 && bal >= 70 ) {
+    console.log( `siz balinigiz C` ) ;
+} else if ( bal < 70 && bal >= 60 ) {
+    console.log( `siz balinigiz D` ) ;
+} else if ( bal < 60 && bal >= 50 ) {
+    console.log( `siz balinigiz F` ) ;
+} else if ( bal < 50 && bal >= 40 ) {
+    console.log( `siz balinigiz E yaxwlab oqing!!! ` ) ;
+} else {
+    console.log( `balingiz passss!!!` ) ;
+}
+alert(`hello world`)
+
+
